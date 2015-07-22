4245e918709be28669765f3261e6108cd6063bfd
SmBe19 <bsgame27@gmail.com>
2015-07-22 18:39:38 +0200

    Implement EvoCreoBot

--- EvoCreoBot/EvoCreoBot.py
+++ EvoCreoBot/EvoCreoBot.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
 """
-An empty bot to be able to start a new bot fast.
+A bot that posts a link to a wiki (for /r/evocreo)
 Written by /u/SmBe19
 """
 
 import praw
+import re
 import time
 import OAuth2Util
 
@@ -18,7 +19,12 @@ USERAGENT = ""
 SUBREDDIT = ""
 
 # The time in seconds the bot should sleep until it checks again.
-SLEEP = 60*60
+SLEEP = 60
+
+# The regex used to find comments to reply
+COMMENT_REGEX = re.compile(r"\{(.*?)\} is my favorite Creo\.")
+
+ANSWER = "More information about [{0}](http://evocreo.wikia.com/wiki/{0})."
 
 # ### END USER CONFIGURATION ### #
 
@@ -33,7 +39,7 @@ try:
 	SUBREDDIT = bot.subreddit
 except ImportError:
 	pass
-	
+
 def read_config_done():
 	done = []
 	try:
@@ -44,41 +50,53 @@ def read_config_done():
 	except OSError:
 		print(DONE_CONFIGFILE, "not found.")
 	return done
-	
+
 def write_config_done(done):
 	with open(DONE_CONFIGFILE, "w") as f:
 		for d in done:
 			if d:
 				f.write(d + "\n")
-	
+
 # main procedure
 def run_bot():
 	r = praw.Reddit(USERAGENT)
 	o = OAuth2Util.OAuth2Util(r)
 	o.refresh()
 	sub = r.get_subreddit(SUBREDDIT)
-	
+
 	print("Start bot for subreddit", SUBREDDIT)
-	
+
 	done = read_config_done()
-	
+
 	while True:
 		try:
-			pass
-			
+			o.refresh()
+			sub.refresh()
+
+			for comment in sub.get_comments():
+				if comment.name not in done:
+					answer = ""
+					for match in COMMENT_REGEX.finditer(comment.body):
+						answer += ANSWER.format(match.group(1)) + "\n"
+
+					if len(answer) > 0:
+						print("reply")
+						comment.reply(answer)
+						done.append(comment.name)
+
 		# Allows the bot to exit on ^C, all other exceptions are ignored
 		except KeyboardInterrupt:
 			break
 		except Exception as e:
 			print("Exception", e)
-			
+
 		write_config_done(done)
 		print("sleep for", SLEEP, "s")
 		time.sleep(SLEEP)
-		
+
 	write_config_done(done)
-	
-	
+
+
 if __name__ == "__main__":
 	if not USERAGENT:
 		print("missing useragent")
---
+++ EvoCreoBot/README.md
@@ -0,0 +1,2 @@
+# EvoCreo Bot
+A bot that posts a link to a wiki (for http://reddit.com/r/evocreo).
